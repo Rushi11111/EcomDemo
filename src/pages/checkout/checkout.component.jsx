95e2b31002c071b7178a170a0bc49912a961f72a
Rushi11111 <rushipatel11111@gmail.com>
2022-01-03 10:04:49 +0530

    added checkout item component to checkoutpage

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,45 +4,50 @@ import './checkout.styles.scss';
 import {connect} from 'react-redux';
 import {selectCartItems} from "../../redux/cart/cart.selector";
 import {selectCartTotal} from "../../redux/cart/cart.selector";
+import CheckoutItem from "../../components/checkout-item/checkpout-item.component";
 
-const CheckoutPage = ({cartItems, cartTotal}) => (
-    <div className={'checkout-page'}>
-        <div className={'checkout-header'}>
-            <div className={'header-block'}>
+
+const CheckoutPage = ({cartItems, cartTotal}) => {
+    console.log(cartItems.map(cartItem => (cartItem)))
+    return (
+        <div className={'checkout-page'}>
+            <div className={'checkout-header'}>
+                <div className={'header-block'}>
                 <span>
                     Product
                 </span>
-            </div>
-            <div className={'header-block'}>
+                </div>
+                <div className={'header-block'}>
                 <span>
                     Description
                 </span>
-            </div>
-            <div className={'header-block'}>
+                </div>
+                <div className={'header-block'}>
                 <span>
                     Quantity
                 </span>
-            </div>
-            <div className={'header-block'}>
+                </div>
+                <div className={'header-block'}>
                 <span>
                     Price
                 </span>
-            </div>
-            <div className={'header-block'}>
+                </div>
+                <div className={'header-block'}>
                 <span>
                     Remove
                 </span>
+                </div>
+            </div>
+
+            {
+                cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} {...cartItem}/>))
+            }
+            <div className={'total'}>
+                TOTAL: ${cartTotal}
             </div>
         </div>
-
-        {/*{*/}
-        {/*    cartItems.map(cartItem => (<div>cartItem.name</div>))*/}
-        {/*}*/}
-        <div className={'total'}>
-            TOTAL: ${cartTotal}
-        </div>
-    </div>
-)
+    )
+}
 
 const mapStateToProps = (state) => ({
     cartItems: selectCartItems(state),
